67adcdbef7d905e9407ba09499ce148f6cb3683d
Alfredo Costa <alfredocosta@live.com>
2024-01-11 15:48:53 -0300

    Create example page

---
+++ src/app/exemplo/page.tsx
@@ -0,0 +1,10 @@
+export default function Exemplo() {
+    return (
+        <main>
+            <h2>Olá, mundo!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam commodi,
+                deserunt dolore eaque eligendi esse eveniet harum illum ipsum magnam, nobis
+                repellat repudiandae sed similique, sint vitae voluptas. Nemo.</p>
+        </main>
+    )
+}

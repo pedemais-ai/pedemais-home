dc8e4a54a98b0afe52f1545cc7b73a1ab5e1b16b
Alfredo Costa <alfredocosta@live.com>
2024-04-20 11:55:07 -0300

    fix: add lead message box

--- src/components/register/Lead.tsx
+++ src/components/register/Lead.tsx
@@ -13,8 +13,15 @@ export default function Lead() {
     }
 
     return (<>
-        <h5 className="mb-2 text-red custom-bold">PedeMais.Ai</h5>
-        <p className="lead mb-4">Obrigado por se cadastrar! Entraremos em contato em breve</p>
+        <div style={{
+            borderRadius: "8px",
+            boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.1)",
+            marginBottom: "20px",
+            border: "1px solid #ccc"
+        }}>
+            <h5 className="mb-2 text-red custom-bold">PedeMais.Ai</h5>
+            <p className="lead mb-4">Obrigado por se cadastrar! Entraremos em contato em breve</p>
+        </div>
     </>);
 
     // return <LeadPassword leadId={leadId}/>;
